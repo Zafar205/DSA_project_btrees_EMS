a65de387597403f9cbec0bd4199ab8d8d1900357
Zafar205 <mz09206@st.habib.edu.pk>
2024-10-12 01:46:39 +0500

    hmmm

--- EMS/project/myApp/admin.py
+++ EMS/project/myApp/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Employee, Role, Department
-admin.site.register(Employee)
-admin.site.register(Department)
-admin.site.register(Role)
+# from .models import Employee, Role, Department
+# admin.site.register(Employee)
+# admin.site.register(Department)
+# admin.site.register(Role)
 # Register your models here.
