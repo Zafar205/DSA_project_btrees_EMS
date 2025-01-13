289d45c7b33bb64c2b3424e9c09a17ad66268b63
Zafar205 <142920742+Zafar205@users.noreply.github.com>
2025-01-13 13:14:15 +0500

    Update admin.py

--- EMS/project/myApp/admin.py
+++ EMS/project/myApp/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-# from .models import Employee, Role, Department
-# admin.site.register(Employee)
-# admin.site.register(Department)
-# admin.site.register(Role)
-# Register your models here.
+from .models import Employee, Role, Departmentadmin.site.register(Employee)
+admin.site.register(Department)
+admin.site.register(Role)
+Register your models here.
